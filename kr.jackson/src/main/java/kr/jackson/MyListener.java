8792903e290c95939b565d9e206575304de9525e
Jackson Hong <jacksonhong0111@gmail.com>
2022-04-13 21:28:02 +0900

    [add] Tomcat server config

---
+++ kr.jackson/src/main/java/kr/jackson/MyListener.java
@@ -0,0 +1,19 @@
+package kr.jackson;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Context Initialized");
+        sce.getServletContext().setAttribute("name", "jackson");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Context Destroyed");
+    }
+}
