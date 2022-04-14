93c1ca58a30d949f32275a71d41ebef928dbfc17
Jackson Hong <jacksonhong0111@gmail.com>
2022-04-14 22:27:25 +0900

    [add] Servlet + SpringMVC

--- kr.jackson/src/main/java/kr/jackson/WebConfig.java
+++ kr.jackson/src/main/java/kr/jackson/WebConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class)) // controller는 bean으로 등록하지 않음
-public class AppConfig {
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
+public class WebConfig {
 }
